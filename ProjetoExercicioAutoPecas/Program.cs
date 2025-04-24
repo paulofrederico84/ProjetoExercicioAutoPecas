f34460cf30718b23b6fc3701004867ecee68330d
Paulo Euclides Frederico <69606562+paulofrederico84@users.noreply.github.com>
2025-04-23 21:28:24 -0300

    Alteração e teste da classe program

--- ProjetoExercicioAutoPecas/Program.cs
+++ ProjetoExercicioAutoPecas/Program.cs
@@ -8,6 +8,27 @@ namespace ProjetoExercicioAutoPecas
         {
             MenuPrincipal menuPrincipal = new MenuPrincipal();
             menuPrincipal.Main();
+
+            string caminhoBanco = BuscarCaminhoBanco();
+            
+            if (!File.Exists(caminhoBanco))
+            {
+                Console.WriteLine("Arquivo de banco de dados não encontrado.");
+                return;
+            }
+            
+            string[] linhas = File.ReadAllLines(caminhoBanco);
+            foreach (string linha in linhas)
+            {
+                Console.WriteLine(linha);
+            }
+            
+        }
+
+        private static string BuscarCaminhoBanco()        
+        {            
+            return @"C:\Users\Paulo\OneDrive\Área de Trabalho\AULA PROGRAMAÇÃO\Entra21\C#\ProjetoExercicio\ProjetoExercicioAutoPecas\BancoDados";
         }
     }
+    
 }
